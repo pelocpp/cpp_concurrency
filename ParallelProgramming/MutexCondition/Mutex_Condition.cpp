6e92649b971daed9df1b1017cc6d9496bd8bda9e
Peter Loos <peter.loos@gmx.de>
2020-03-01 17:18:51 +0100

    bug fix lock and notify

--- ParallelProgramming/MutexCondition/Mutex_Condition.cpp
+++ ParallelProgramming/MutexCondition/Mutex_Condition.cpp
@@ -14,43 +14,32 @@ namespace MutexDemo {
     constexpr long NumIterations = 5;
 
     std::mutex mutex;
-    std::condition_variable cv;
+    std::condition_variable condition;
 
     void function(int value) {
 
         std::thread::id tid = std::this_thread::get_id();
-        Logger::log(std::cout, "tid:  ", tid);
+        Logger::log(std::cout, "TID:  ", tid);
 
         std::unique_lock<std::mutex> lock{ mutex };
-        cv.wait(lock, []() {
-                std::cout << "Waking up ..." << std::endl;
-                return true;
-            }
-        );
+        condition.wait(lock);
 
-        Logger::log(std::cout, "Done Thread");
+        Logger::log(std::cout, "Done Thread ", tid);
     }
 
     void test() {
-        Logger::log(std::cout, "Begin");
-
         std::thread::id mainTID = std::this_thread::get_id();
-        Logger::log(std::cout, "main: ", mainTID);
+        Logger::log(std::cout, "Begin Main: ", mainTID);
 
-        std::thread t1;
-        std::thread t2;
+        std::thread t1 = std::thread(function, 1);;
+        std::thread t2 = std::thread(function, 2);;
 
-        {
-            std::scoped_lock<std::mutex> lock(mutex);
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        condition.notify_one();
 
-            t1 = std::thread (function, 1);
-            t2 = std::thread (function, 2);
 
-            using namespace std::chrono_literals;
-            std::this_thread::sleep_for(3s);
-        }
-
-        cv.notify_all();
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        condition.notify_one();
 
         t1.join();
         t2.join();
@@ -59,12 +48,12 @@ namespace MutexDemo {
     }
 }
 
-int main_cond()
-{
-    using namespace MutexDemo;
-    test();
-    return 1;
-}
+//int main()
+//{
+//    using namespace MutexDemo;
+//    test();
+//    return 1;
+//}
 
 // ===========================================================================
 // End-of-File
