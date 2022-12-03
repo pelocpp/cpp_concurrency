46ff325b4b120c62efe4f7f244cdfcd380c5d9a0
Peter Loos <peter.loos@gmx.de>
2022-12-03 11:46:44 +0100

    mutex object can now be declared without 'static' keyword

--- Programs/03_MutualExclusion/Simple_Mutex.cpp
+++ Programs/03_MutualExclusion/Simple_Mutex.cpp
@@ -13,22 +13,27 @@ namespace SimpleMutexDemo
     {
     private:
         const size_t NumIterations{ 100000 };
+        int m_id;
 
         static std::mutex s_mutex;
 
-        int m_id;
+        // Note: remove static
+        // a) Line above
+        // b) put 'std::mutex Counter::s_mutex{};' into comments
+        // c) add 'mutable' to s_mutex declaration
 
     public:
         Counter(int id) : m_id{ id } {}
 
-        void operator()() const
-        {
+        void run() const {
+
             for (size_t i{}; i != NumIterations; ++i)
             {
                 {
                     // <== remove comment from next line to demonstrate scattered output
                     //std::scoped_lock<std::mutex> raii{ s_mutex };
                     //std::scoped_lock raii{ s_mutex };  // C++ 17 (automatic type deduction)
+
                     std::cout << "Counter " << m_id << ": i=" << i << "\n";
                 }
 
@@ -45,10 +50,14 @@ void test()
 {
     using namespace SimpleMutexDemo;
 
+    Counter counter1{ 1 };
+    Counter counter2{ 2 };
+    Counter counter3{ 3 };
+
     // run 3 threads ...
-    std::thread t1{ Counter{ 1 } };
-    std::thread t2{ Counter{ 2 } };
-    std::thread t3{ Counter{ 3 } };
+    std::thread t1{ &Counter::run, &counter1 };
+    std::thread t2{ &Counter::run, &counter2 };
+    std::thread t3{ &Counter::run, &counter3 };
 
     // wait for threads to finish
     t1.join();
