106aa18bcc7c6d1906a678c37a742e9af53324cd
Peter Loos <peter.loos@gmx.de>
2026-05-22 11:47:33 +0200

    fixed typos

--- Programs/34_ThreadPool/Program.cpp
+++ Programs/34_ThreadPool/Program.cpp
@@ -2,27 +2,29 @@
 // Program.cpp - Thread Pool
 // ===========================================================================
 
-extern void test_concurrency_thread_pool01();   // just launching ... and stopping the thread pool
-extern void test_concurrency_thread_pool02();   // launching 5 almost empty tasks
-extern void test_concurrency_thread_pool03();   // launching many tasks ... and working on the same global variable (by address)
-extern void test_concurrency_thread_pool04();   // launching many tasks ... and working on an atomic variable (by address)
-extern void test_concurrency_thread_pool05();   // launching many tasks ... and working on an atomic variable (by reference)
-extern void test_concurrency_thread_pool10();   // computing prime numbers, using free function
-extern void test_concurrency_thread_pool11();   // computing prime numbers, using lambda
-extern void test_concurrency_thread_pool12();   // computing prime numbers, using lambda
+extern void test_concurrency_thread_pool01();    // just launching ... and stopping the thread pool
+extern void test_concurrency_thread_pool02();    // launching 5 almost empty tasks
+
+extern void test_concurrency_thread_pool03();    // launching many tasks ... and working on the same global variable (by address)
+extern void test_concurrency_thread_pool04();    // launching many tasks ... and working on an atomic variable (by address)
+extern void test_concurrency_thread_pool05();    // launching many tasks ... and working on an atomic variable (by reference)
+
+extern void test_concurrency_thread_pool10();    // computing prime numbers, using free function (minimum variant)
+extern void test_concurrency_thread_pool11();    // computing prime numbers, using lambda (printing values to the console) 
+extern void test_concurrency_thread_pool12();    // computing prime numbers, using lambda (returning computed values with future objects)
 
 int main()
 {
-    test_concurrency_thread_pool01();  // just launching ... and stopping the thread pool
-    test_concurrency_thread_pool02();  // launching 5 almost empty tasks
+    test_concurrency_thread_pool01();            // just launching ... and stopping the thread pool
+    test_concurrency_thread_pool02();            // launching 5 almost empty tasks
 
-    test_concurrency_thread_pool03();  // launching many tasks ... and working on the same global variable (by address)
-    test_concurrency_thread_pool04();  // launching many tasks ... and working on an atomic variable (by address)
-    test_concurrency_thread_pool05();  // launching many tasks ... and working on an atomic variable (by reference)
+    test_concurrency_thread_pool03();            // launching many tasks ... and working on the same global variable (by address)
+    test_concurrency_thread_pool04();            // launching many tasks ... and working on an atomic variable (by address)
+    test_concurrency_thread_pool05();            // launching many tasks ... and working on an atomic variable (by reference)
 
-    test_concurrency_thread_pool10();   // computing prime numbers (^minimum variant)
-    test_concurrency_thread_pool11();   // computing prime numbers (printing values to the console) 
-    test_concurrency_thread_pool12();   // computing prime numbers (returning computed values with future objects)
+    test_concurrency_thread_pool10();            // computing prime numbers (minimum variant)
+    test_concurrency_thread_pool11();            // computing prime numbers (printing values to the console) 
+    test_concurrency_thread_pool12();            // computing prime numbers (returning computed values with future objects)
 
     return 0;
 }
